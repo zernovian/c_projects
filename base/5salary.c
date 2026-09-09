2fca3affb9b1c2e385f4811116b245580539c186
zernovian <saksham210708@gmail.com>
2026-09-09 21:11:23 +0530

    Fix

--- base/5salary.c
+++ base/5salary.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-int main() {
+
+int main(void) {
     char name[50];
-    float bs,hra,da,gross_salary;
-    
-    printf("enter employee name: ");
-    scanf(" %[^\n]s",name);
-    printf("enter basic salary, hra, da: ");
-    scanf("%f %f %f", &bs, &hra, &da);
+    float bs, hra, da, gross_salary;
 
-    gross_salary=bs+hra+da;
+    printf("Enter employee name: ");
+    scanf(" %49[^\n]", name);
 
-    printf("\nemployee: %s\ngross_salary: %.2f\n",name, gross_salary);
+    printf("Enter basic salary, HRA, and DA: ");
+    if (scanf("%f %f %f", &bs, &hra, &da) != 3) {
+        printf("Invalid salary input.\n");
+        return 1;
+    }
+
+    gross_salary = bs + hra + da;
+
+    printf("\nEmployee: %s\nGross salary: %.2f\n", name, gross_salary);
+
     return 0;
-}  
+}
